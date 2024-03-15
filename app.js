d2d3c1f64e838726a9bfed55ce0ed958a73ee8c8
krisneiras <kristygns21@gmail.com>
2024-03-15 15:46:08 +0100

    installing cors

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ import express from "express";
 import {DB_PORT} from "./config.js"
 import SculptureRouter from "./routes/SculptureRouter.js";
 import chalk from "chalk";
+import cors from "cors"
 
 export const app = express();
+
+app.use(cors());
 
 app.use(express.json()); // para que la aplicacion pueda soportar formato json desde postman (Middleware para parsear el cuerpo de las solicitudes en formato JSON)
 
