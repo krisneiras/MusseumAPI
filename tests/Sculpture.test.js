9156533081c692cd0628e94136be5feecfcdeab8
krisneiras <kristygns21@gmail.com>
2024-03-19 09:29:23 +0100

    changing tests structure

--- tests/Sculpture.test.js
+++ tests/Sculpture.test.js
@@ -5,51 +5,105 @@ import SculptureModel from '../models/SculptureModel';
 
 const api = request(app); // request nos permite hacer solicitudes a la app
 
-describe('Testing CRUD sculptures', ()=>  {
+describe('Testing CRUD in sculptures API', ()=>{
 
-/*     beforeAll( async () => {
-        await connection_db.sync();
-    });
- */
-    afterAll(async () =>{
-        await SculptureModel.destroy({where: {title:'hola'}});
-        await connection_db.close();
-        server.close();
-    });
+    describe('Get request from sculptures api', ()=>{
+        let response;
+        beforeEach( async() =>{
+            response = await api.get('/api');
+        });
     
-
-    test('Response body must be an array and then show 200 status', async() => {
-    const response = await api.get('/api');
-    expect(Array.isArray(response.body)).toBe(true); //que objeto quiero recibir; modificar
-    expect(response.status).toBe(200); 
-    });
-
-    test('Post response should be an object and return status 201', async () => {
-        const response = await api.post('/api').send({
+        test('Response body must be an array', async() => {
+            expect(Array.isArray(response.body)).toBe(true);
+        });
+    
+        test('Respons status should be 200', () => {
+            expect(response.status).toBe(200);
+        })
+    })
+    
+    describe('Put request on sculptures api', ()=>{
+        let newSculpture = {}
+        let response;
+    
+        beforeEach( async() =>{
+            newSculpture = await SculptureModel.create({
                 "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Michelangelo%27s_David_-_right_view_2.jpg/270px-Michelangelo%27s_David_-_right_view_2.jpg",
-                "title": "hola",
+                "title": "test",
                 "author": "test",
                 "material": "test",
                 "year": 1977,
                 "location": "test"
         });
-        expect(typeof response.body).toBe('object');
-        expect(response.status).toBe(201); 
+    
     });
-
-    test('Put request should update a sculpture value and return OK', async () => {
-
-        const id  = 42;
-
-        const response = await api.put(`/api/${id}`).send({
+    
+        test('Put request should update a sculpture value and return OK', async () => {
+            const response = await api.put(`/api/${newSculpture.id}`).send({
+                "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Michelangelo%27s_David_-_right_view_2.jpg/270px-Michelangelo%27s_David_-_right_view_2.jpg",
+                "title": "test",
+                "author": "test",
+                "material": "test",
+                "year": 1977,
+                "location": "test"
+            });
+            expect(response.status).toBe(200);
+        });
+    
+        afterAll(async () =>{
+            await SculptureModel.destroy({where: {title:'test'}});
+        });
+    })
+    
+    describe('POST in sculptures api', ()=>{
+    
+    
+       test('Response should return status 201 and type object', async()=>{ 
+        
+        const response = await api.post('/api').send({
             "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Michelangelo%27s_David_-_right_view_2.jpg/270px-Michelangelo%27s_David_-_right_view_2.jpg",
             "title": "test",
-            "author": "Carlos",
+            "author": "test",
             "material": "test",
             "year": 1977,
             "location": "test"
         });
-        expect(response.status).toBe(200);
+        
+        expect(typeof response.body).toBe('object');
+        expect(response.status).toBe(201); 
+        
+    })
+    afterAll(async () =>{
+        await SculptureModel.destroy({where: {title:'test'}});
     });
-
-});
+    })
+    
+    describe('DELETE request sculptures api', ()=>  {
+        let newSculpture = {}
+        let response;
+        beforeEach( async() =>{
+            newSculpture = await SculptureModel.create({
+                "image_url": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Michelangelo%27s_David_-_right_view_2.jpg/270px-Michelangelo%27s_David_-_right_view_2.jpg",
+                "title": "test",
+                "author": "test",
+                "material": "test",
+                "year": 1977,
+                "location": "test"
+        });
+    
+        response = await api.delete(`/api/${newSculpture.id}`);
+        });
+    
+        test('Should return status 201', () => {
+            expect(response.status).toBe(201);
+        });
+    
+    });
+    
+    afterAll( () =>{
+        connection_db.close();
+        server.close();
+    });
+    
+    })
+    
