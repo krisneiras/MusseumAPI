7b06de953ea38588a864016285663c4e90650ae6
krisneiras <kristygns21@gmail.com>
2024-05-10 14:44:13 +0200

    feat(config/connection_db):try-to-connect-to-clevercloud

--- config.js
+++ config.js
@@ -6,5 +6,6 @@ export const DB_PASSWORD = process.env.DB_PASSWORD;
 export const DB_PORT = process.env.DB_PORT || 5000;
 export const DB_TEST_NAME = process.env.DB_TEST_NAME;
 export const NODE_ENV = process.env.NODE_ENV
+export const DB_HOST = process.env.DB_HOST
 
 
--- database/connection_db.js
+++ database/connection_db.js
@@ -1,11 +1,11 @@
 import { Sequelize } from "sequelize";
-import { DB_DEV_NAME, DB_USER, DB_PASSWORD, DB_TEST_NAME, NODE_ENV } from "../config.js";
+import { DB_DEV_NAME, DB_USER, DB_PASSWORD, DB_TEST_NAME, NODE_ENV, DB_HOST } from "../config.js";
 
 const DB_NAME = NODE_ENV === 'test' ? DB_TEST_NAME : DB_DEV_NAME;  //Escogiendo nuestra conexion con la base de datos de test
 // ? es un if y : un else.
 
 const connection_db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: 'localhost',
+  host: DB_HOST,
   dialect: "mysql"
 });
 
